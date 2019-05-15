e7776bdbcb7b3ef04e40e4735d6dc8fcfea38597
JanetGM <antaly.146@gmail.com>
2019-05-15 09:42:50 -0500

    pintar data de compra en componente  tech

--- src/components/FormAprobTec/InfoVehiculo.js
+++ src/components/FormAprobTec/InfoVehiculo.js
@@ -1,52 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { useCollection } from 'react-firebase-hooks/firestore';
 import InfoRepuestos from './InfoRepuestos';
 import InfoTaller from './InfoTaller';
-import { useCollection } from 'react-firebase-hooks/firestore';
 import db from '../../lib/firestore';
-
 
 const InfoVehiculo = () => {
   const { error, loading, value } = useCollection(
-    db.collection('modelo_auto'),
+    db.collection('compras'),
   );
-  return(<div>
-  {/* {error && <strong>Error: {error}</strong>}
-  {loading && <span>Collection: Loading...</span>}
-  {value && (
-    <span>
-         {value.docs.map(doc => ( 
+
+        return (
             <div className=" card datos-vehiculo">
-            <div className="box-left">
-            <h4>DATOS DE VEHÍCULO:</h4>
-            <br></br>
-            <div className="info"><span className ="font-weight-bold">Marca: </span>
-            <span><span >
-            {doc.data().marca}</span></span>
-            </div>
-            <div className="info">
-              <span className="font-weight-bold">Modelo: </span><span>aqui</span>
-            </div>
-            <div className="info"><span className="font-weight-bold">N° de motor: </span>
-              <span>aqui</span>
-            </div>
-            </div>
-              <div className="box-r">
+             {error && <strong>Error: {error}</strong>}
+            {loading && <span>Collection: Loading...</span>}
+           {value && (
+              <span>{value.docs.map(doc => (
+                <div className="box-left">
+                <h4>DATOS DE VEHÍCULO:</h4>
+                <br></br>
+                <div className="info">
+                <span className ="font-weight-bold">
+                Marca:</span>
+                <span>{(doc.data().marca)}</span>
+                <div className="info">
+                <span className="font-weight-bold">Modelo: </span><span>{(doc.data().modelo)}</span>
+                </div>
+                <div className="info"><span className="font-weight-bold">N° de motor:{(doc.data().motor)} </span>
+                <span>aqui</span>
+                </div>
+                <span><span >
+                aqui</span></span>
+                </div>
+                <div className="box-r">
                 <div className="info"><span className="font-weight-bold"> Año: </span>
-                    <span>aqui</span>
+                    <span>{(doc.data().anio)}</span>
                 </div>
                 <div className="info">
                   <span className="font-weight-bold">VIN: </span>
-                  <span>aqui</span>
+                  <span>no se sabe</span>
                 </div>
-                <div className="info"><span className="font-weight-bold">Sede: </span><span>aqui</span>
-                </div>
+                
                 <p>Descargar imágenes</p>
                 <button className="btn btn-red" type="button"> <img className="download" src="https://i.ibb.co/kJ3CGnF/download.png"></img></button>
                 </div>
+              </div>
+                ))}
                 <InfoTaller />
                 <InfoRepuestos />
-            </div> */}
-    
-     }
-</div>)}
+              </span>
+                
+            
+        )}
+        </div> 
+    );
+}
 export default InfoVehiculo;
